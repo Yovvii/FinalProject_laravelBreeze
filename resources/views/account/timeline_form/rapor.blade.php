a2ed676dc2b62698cd0c1d6f13e771dacccd5098
Yovvii <yomanfi@gmail.com>
2025-08-28 14:52:37 +0700

    Navbar Update

--- resources/views/account/timeline_form/rapor.blade.php
+++ resources/views/account/timeline_form/rapor.blade.php
@@ -9,189 +9,33 @@
         <div>
             <p class="font-bold mb-2">Data Nilai Rapor Semester 1</p>
             <div class="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
+                <div class="sm:col-span-6 xl:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
                     <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Matematika</label>
                     <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
                     class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
                 </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
+                <div class="sm:col-span-6 xl:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
                     <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Indonesia</label>
                     <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
                     class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
                 </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
+                <div class="sm:col-span-6 xl:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
                     <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Inggris</label>
                     <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
                     class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
                 </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
+                <div class="sm:col-span-6 xl:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
                     <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Alam</label>
                     <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
                     class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
                 </div>
-                {{-- <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
+                {{-- <div class="sm:col-span-6 xl:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
                     <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Sosial</label>
                     <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
                     class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
                 </div> --}}
-                <div class="sm:col-span-2 relative px-3 pt-3 bg-white rounded-lg">
+                <div class="sm:col-span-6 xl:col-span-2 relative px-3 pt-3 bg-white rounded-lg">
                     <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700 font-bold">Upload Scan Rapor Semester 1 (PDF)</label>
-                    <input type="file" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-            </div>
-        </div>
-</div>
-
-
-<div class="border bg-blue-100 p-6 rounded-lg mt-4">
-        <div>
-            <p class="font-bold mb-2">Data Nilai Rapor Semester 2</p>
-            <div class="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Matematika</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Indonesia</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Inggris</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Alam</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                {{-- <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Sosial</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div> --}}
-                <div class="sm:col-span-2 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700 font-bold">Upload Scan Rapor Semester 2 (PDF)</label>
-                    <input type="file" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-            </div>
-        </div>
-</div>
-
-
-<div class="border bg-blue-100 p-6 rounded-lg mt-4">
-        <div>
-            <p class="font-bold mb-2">Data Nilai Rapor Semester 3</p>
-            <div class="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Matematika</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Indonesia</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Inggris</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Alam</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                {{-- <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Sosial</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div> --}}
-                <div class="sm:col-span-2 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700 font-bold">Upload Scan Rapor Semester 3 (PDF)</label>
-                    <input type="file" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-            </div>
-        </div>
-</div>
-
-
-<div class="border bg-blue-100 p-6 rounded-lg mt-4">
-        <div>
-            <p class="font-bold mb-2">Data Nilai Rapor Semester 4</p>
-            <div class="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Matematika</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Indonesia</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Inggris</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Alam</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                {{-- <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Sosial</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div> --}}
-                <div class="sm:col-span-2 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700 font-bold">Upload Scan Rapor Semester 4 (PDF)</label>
-                    <input type="file" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-            </div>
-        </div>
-</div>
-
-
-<div class="border bg-blue-100 p-6 rounded-lg mt-4">
-        <div>
-            <p class="font-bold mb-2">Data Nilai Rapor Semester 5</p>
-            <div class="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Matematika</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Indonesia</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Bahasa Inggris</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Alam</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div>
-                {{-- <div class="sm:col-span-1 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700">Ilmu Pengetahuan Sosial</label>
-                    <input type="number" name="nama_lengkap" id="nama_lengkap" value="0"
-                    class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
-                </div> --}}
-                <div class="sm:col-span-2 relative px-3 pt-3 bg-white rounded-lg">
-                    <label for="nama_lengkap" class="absolute left-3 px-1 text-xs bg-white text-gray-700 font-bold">Upload Scan Rapor Semester 5 (PDF)</label>
                     <input type="file" name="nama_lengkap" id="nama_lengkap" value="0"
                     class="block w-full px-1 mt-3 text-base text-gray-800 border-0 focus:ring-0 focus:outline-none">
                 </div>
